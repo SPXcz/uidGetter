53fc8439d93e7a9d910bb7594ef10935e808f9c3
Ondrej Chudacek <ondrachu@gmail.com>
2021-03-12 11:42:31 +0100

    Works on added solr URL

--- uidGetter.py
+++ uidGetter.py
@@ -1,7 +1,16 @@
 import json
 import requests
 
-query = "http://solr.dk-back.infra.mzk.cz/solr/kramerius/select?q=collection%3A%22vc%3Ab1adb6f0-01db-45fd-bc4a-d36fa3eab050%22&rows=50&wt=json"
+query = "http://solr.dk-back.infra.mzk.cz/solr/kramerius/select?q=details%3A%2F.*TitlePage.*%2F%20AND%20collection%3A%22vc%3Ab1adb6f0-01db-45fd-bc4a-d36fa3eab050%22&wt=json"
+response = requests.get(query)
+noRows = response.json()['response']['numFound']
+
+query = "http://solr.dk-back.infra.mzk.cz/solr/kramerius/select?q=details%3A%2F.*TitlePage.*%2F%20AND%20collection%3A%22vc%3Ab1adb6f0-01db-45fd-bc4a-d36fa3eab050%22&rows={}&wt=json".format(noRows)
 response = requests.get(query)
 
-print(response.json())
+
+
+with open('files/uids.txt', 'w+') as f:
+    for resp in response.json()['response']['docs']:
+        f.write(resp['root_pid']+"\n")
+print("Done! UIDs are in your file")
